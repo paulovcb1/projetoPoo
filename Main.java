cf4f4c098a9a5ee29f62e0238c58bed9d311ad0b
VictorPraca <victorpraca@gmail.com>
2024-11-19 15:13:21 -0300

    Update zezin

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -9,7 +7,13 @@ public class Main {
         System.out.println("matricula: ");
         int matricula = sc.nextInt();
 
-        Aluno p1 = new Aluno("12/10/2030", 14, "145203", "victor hayakawa campos", matricula, 8, "vespertino", "tarde", 15);
-        p1.exibirInfo();
+        Aluno aluno1 = new Aluno("12/10/2030", 14, "145203", "victor hayakawa campos", matricula, 8, "vespertino", "tarde", 15);
+        aluno1.exibirInfo();
+
+        System.out.println("----------------------------------------------");
+
+        Funcionario func1 = new Funcionario("07/11/2001", 23, "145203", "Girafales", "Professor", 8025.18);
+        func1.exibirInfo();
+
     }
 }
