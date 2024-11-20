cc2ef0509d06abb2942c006a60604d56517e4654
paulobarbosa <paulovcb1@gmail.com>
2024-11-20 11:16:11 -0300

    reorganizacao

--- MenuGUI.java
+++ MenuGUI.java
@@ -1,97 +1,88 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class MenuGUI extends JFrame {
-    public MenuGUI() {
-        ArrayList <Funcionario> listaFuncionario = new ArrayList<>();
-        ArrayList <Aluno> listaAluno = new ArrayList<>();
 
-        // Configuração da janela principal
+    private ArrayList<Funcionario> listaFuncionario = new ArrayList<>();
+    private ArrayList<Aluno> listaAluno = new ArrayList<>();
+
+    public MenuGUI() {
         setTitle("Menu de Navegação");
         setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
 
-
-        // Botão para Cadastrar Aluno
         JButton btnCadastrarAluno = new JButton("Cadastrar Aluno");
         btnCadastrarAluno.setBounds(100, 50, 200, 30);
-        btnCadastrarAluno.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Aluno novoAluno = new Aluno();
-                novoAluno.catchInfo();
-                listaAluno.add(novoAluno);
-                JOptionPane.showConfirmDialog(null, "Aluno cadastrado com sucesso");
-            }
-        });
+        btnCadastrarAluno.addActionListener(e -> cadastrarAluno());
         add(btnCadastrarAluno);
 
-        //Funcionario
-        JButton btnCadastrarFuncionario = new JButton("Cadastrar Funcionario");
+
+        JButton btnCadastrarFuncionario = new JButton("Cadastrar Funcionário");
         btnCadastrarFuncionario.setBounds(100, 100, 200, 30);
-        btnCadastrarFuncionario.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                Funcionario novofuncionario = new Funcionario();
-                novofuncionario.catchInfo();
-
-                listaFuncionario.add(novofuncionario);
-            }
-        });
+        btnCadastrarFuncionario.addActionListener(e -> cadastrarFuncionario());
         add(btnCadastrarFuncionario);
 
-        JButton btnVerFuncionarios = new JButton("Visualizar Funcionarios");
+
+        JButton btnVerFuncionarios = new JButton("Visualizar Funcionários");
         btnVerFuncionarios.setBounds(100, 150, 200, 30);
-        btnVerFuncionarios.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                if (listaFuncionario.isEmpty()){
-                    JOptionPane.showConfirmDialog(null, "Nenhum funcionario foi cadastrado");
-                }
-                for (Funcionario funcionario1 : listaFuncionario) {
-                    JOptionPane.showConfirmDialog(null, funcionario1);
-
-                }
-            }
-        });
+        btnVerFuncionarios.addActionListener(e -> visualizarFuncionarios());
         add(btnVerFuncionarios);
+
 
         JButton btnVerAlunos = new JButton("Visualizar Alunos");
         btnVerAlunos.setBounds(100, 200, 200, 30);
-        btnVerAlunos.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                if (listaAluno.isEmpty()){
-                    JOptionPane.showConfirmDialog(null, "Nenhum aluno foi cadastrado");
-                }
-                for (Aluno alunos : listaAluno) {
-                    JOptionPane.showConfirmDialog(null, alunos);
-
-                }
-            }
-        });
+        btnVerAlunos.addActionListener(e -> visualizarAlunos());
         add(btnVerAlunos);
 
-        // Botão para Sair
+
         JButton btnSair = new JButton("Sair");
         btnSair.setBounds(100, 250, 200, 30);
-        btnSair.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-
-            }
-        });
+        btnSair.addActionListener(e -> System.exit(0));
         add(btnSair);
 
         setVisible(true);
+    }
+
+    private void cadastrarAluno() {
+        Aluno novoAluno = new Aluno();
+        novoAluno.catchInfo();
+        listaAluno.add(novoAluno);
+        JOptionPane.showMessageDialog(this, "Aluno cadastrado com sucesso!");
+    }
+
+    private void cadastrarFuncionario() {
+        Funcionario novoFuncionario = new Funcionario();
+        novoFuncionario.catchInfo();
+        listaFuncionario.add(novoFuncionario);
+        JOptionPane.showMessageDialog(this, "Funcionário cadastrado com sucesso!");
+    }
+
+    private void visualizarFuncionarios() {
+        if (listaFuncionario.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Nenhum funcionário foi cadastrado.");
+            return;
+        }
+
+        StringBuilder mensagem = new StringBuilder("Funcionários cadastrados:\n");
+        for (Funcionario funcionario : listaFuncionario) {
+            mensagem.append(funcionario).append("\n");
+        }
+        JOptionPane.showMessageDialog(this, mensagem.toString());
+    }
+
+    private void visualizarAlunos() {
+        if (listaAluno.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Nenhum aluno foi cadastrado.");
+            return;
+        }
+
+        StringBuilder mensagem = new StringBuilder("Alunos cadastrados:\n");
+        for (Aluno aluno : listaAluno) {
+            mensagem.append(aluno).append("\n");
+        }
+        JOptionPane.showMessageDialog(this, mensagem.toString());
     }
 
     public static void main(String[] args) {
