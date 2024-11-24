5af1a8df9cdbc727e2b4866f7fd4c69c572c3959
Victor Praça <victorpraca@gmail.com>
2024-11-24 19:55:57 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,14 +1,13 @@
 import javax.swing.*;
 
 public class Funcionario extends Pessoa{
+    private int matricula;
     private String cargo;
     private double salario;
 
-
-
-
-    public Funcionario(String data_nasc, int idade, String cpf, String nome, String cargo, double salario){
+    public Funcionario(String data_nasc, int idade, String cpf, String nome, int matricula, String cargo, double salario){
         super(data_nasc, idade, cpf, nome);
+        this.matricula = matricula;
         this.cargo = cargo;
         this.salario = salario;
     }
@@ -20,10 +19,19 @@ public class Funcionario extends Pessoa{
     public void catchInfo () {
         super.catchInfo();
 
-        this.cargo = JOptionPane.showInputDialog(null, "insira seu cargo");
-        this.salario = Double.parseDouble(JOptionPane.showInputDialog(null, "insira seu salario"));
+        this.cargo = JOptionPane.showInputDialog(null, "Insira seu cargo");
+        this.salario = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira seu salario"));
 
     }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
     public String getCargo() {
         return cargo;
     }
@@ -40,16 +48,14 @@ public class Funcionario extends Pessoa{
         this.salario = salario;
     }
 
-
-
-   @Override
+    @Override
 
     public String toString () {
         return "\n" + '\'' + super.toString() + "\n" +
+                "Matricula: " + matricula + '\'' + "\n" +
                 "Cargo: " + cargo + '\'' + "\n" +
-                "salario: " + salario + "\n";
+                "Salario: " + salario + "\n";
 
-   }
+    }
 
 }
-
